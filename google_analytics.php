702ada7970f590cb5c6e947c3f47f6671a87ed94
Arnaud <ArnaudHocevar@users.noreply.github.com>
2014-12-16 14:41:26 +0100

    Update google_analytics.php
    Fixed issue where analytics ID was not shown, preventing any kind of tracking

--- google_analytics.php
+++ google_analytics.php
@@ -67,8 +67,8 @@ class GoogleAnalytics {
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');\n";
-	
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', '" . getOption(GAConfig::getConfItem('AnalyticsId','property_name')) . "', 'auto');\n";
 			echo GAToolbox::buildAdditionalParams('require');
 			echo GAToolbox::buildAdditionalParams('other');
 			echo GAToolbox::buildAdditionalParams('set');
